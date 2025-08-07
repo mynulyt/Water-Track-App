28c9f506f5b800d28e6be49459a6210ff3623e88
Mynul Alam <mynulalam27@gmail.com>
2025-08-07 22:58:14 +0600

    water track

---
+++ lib/water_track.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WaterTrack extends StatefulWidget {
+  const WaterTrack({super.key});
+
+  @override
+  State<WaterTrack> createState() => _WaterTrackState();
+}
+
+class _WaterTrackState extends State<WaterTrack> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
